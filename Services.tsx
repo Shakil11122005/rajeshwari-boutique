4f76e778b7013cd52e666a0b6a5231d6abbef263
Shakil Bharathi <622123113052@paavai.edu.in>
2026-04-21 23:23:26 +0530

    Updated Services.tsx to replace image src URLs.

---
+++ Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <img src="/public/images/aari-work-1.jpeg" alt="Aari Work 1" />
+            <img src="/public/images/aari-work-2.jpeg" alt="Aari Work 2" />
+            {/* Other contents of the component */}
+        </div>
+    );
+};
+
+export default Services;
