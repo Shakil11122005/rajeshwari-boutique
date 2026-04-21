4ed4b0f9b071cb8e2a48d8195bc846446ba397de
Shakil Bharathi <622123113052@paavai.edu.in>
2026-04-21 14:40:43 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,7 +21,7 @@ export const About = () => (
         </motion.div>
         <motion.div initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true }} className="lg:w-1/2 relative">
           <div className="bg-teal/10 w-full h-[500px] rounded-3xl relative z-0 translate-x-6 translate-y-6 border-2 border-teal/20"></div>
-        <img src="/images/rajeshwari-logo.jpg" alt="Rajeshwari Boutique & Aari Works Logo" className="absolute inset-0 w-full h-[500px] object-contain rounded-3xl z-10 shadow-2xl border-2 border-magenta/20 bg-[#3a0a1a] p-8" />
+        <img src="/images/rajeshwari-logo.jpg" alt="Rajeshwari Boutique & Aari Works Logo" className="absolute inset-0 w-full h-[500px] object-contain rounded-3xl z-10 shadow-2xl border-2 border-magenta/20 bg-[#3a0a1a] p-8"" />
         </motion.div>
       </div>
 
