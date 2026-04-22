a49078f11f46e2f1141af04c283e5afa9fef7283
Shakil Bharathi <622123113052@paavai.edu.in>
2026-04-22 09:17:13 +0530

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -72,7 +72,7 @@ export const Services = () => {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-6 relative z-10">
-           <img src=""WhatsApp Image 2026-04-21 at 10.51.36 PM.jpeg" alt="Aari Work 1" className="w-full h-56 object-cover rounded-2xl border-2 border-gold/20 shadow-xl" referrerPolicy="no-referrer" />
+           <img src="WhatsApp Image 2026-04-21 at 10.51.36 PM.jpeg" alt="Aari Work 1" className="w-full h-56 object-cover rounded-2xl border-2 border-gold/20 shadow-xl" referrerPolicy="no-referrer" />
            <img src="WhatsApp Image 2026-04-21 at 10.51.37 PM.jpeg" alt="Aari Work 2" className="w-full h-56 object-cover rounded-2xl border-2 border-gold/20 shadow-xl" referrerPolicy="no-referrer" />
           </div>
         </div>
